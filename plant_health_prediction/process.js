e49846e9fd7a810adb542b14af006f8218d9e065
Naimish Bhuva <naimishbbhuva@gmail.com>
2024-07-31 13:19:24 +0530

    fix: model working, tested ok

--- plant_health_prediction/process.js
+++ plant_health_prediction/process.js
@@ -1,29 +1,26 @@
-// const imageInput = document.getElementById('image-input');
-// const submitButton = document.getElementById('submit-button');
+const imageInput = document.getElementById("imageupload");
+const submitButton = document.getElementById("submit-button");
+const predictionText = document.getElementById("prediction-text");
+const confidenceText = document.getElementById("confidence-text");
 
-// submitButton.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     const imageFile = imageInput.files[0];
-//     // You can add your logic here to handle the image file
-//     console.log(imageFile);
-// });
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const imageFile = imageInput.files[0];
+  const formData = new FormData();
+  formData.append("image", imageFile);
 
-import "./Backend.cjs";
-
-submitButton.addEventListener('click', (e) => {
-e.preventDefault();
-const imageFile = imageInput.files[0];
-const reader = new FileReader();
-reader.onload = () => {
-const imageData = reader.result;
-// Send the image data to the backend for comparison
-fetch('/compareImage', {
-method: 'POST',
-headers: { 'Content-Type': 'application/json' },
-body: JSON.stringify({ imageData }),
-})
-.then(response => response.json())
-.then(data => console.log(data.match));
-};
-reader.readAsDataURL(imageFile);
+  fetch("http://127.0.0.1:8080/predict", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.prediction);
+      const [predictedClass, confidence] = data.prediction;
+      predictionText.innerHTML = `Prediction: ${predictedClass}`;
+      confidenceText.innerHTML = `Confidence: ${confidence}%`;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
